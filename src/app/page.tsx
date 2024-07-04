2b64fe423eac891bac68c4beabac23fdf4e9421d
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-07-05 01:39:27 +0900

    chore: 불필요한 주석 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-'use client';
-
-import { useRouter } from 'next/navigation';
 import s from './page.module.scss';
 
 export default function Home() {
-  const router = useRouter();
-
-  const isCookieExist = () => {
-    return document.cookie.includes('token');
-  };
-
-  if (!isCookieExist()) router.push('/entry');
-
   return <main className={s.container}>wagu book</main>;
 }
