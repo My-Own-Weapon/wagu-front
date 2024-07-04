f8eb510150383f421ff82e8b5227d52574e913e1
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-07-05 01:39:01 +0900

    feat: added type

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface LoginUserInputs {
+  username: string;
+  password: string;
+}
+
+export interface SignupDetails {
+  username: string;
+  password: string;
+  passwordConfirm: string;
+  name: string;
+  phoneNumber: string;
+}
