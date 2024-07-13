27ba35977076ac066369be44cc1e3f5977017b82
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-07-13 17:42:51 +0900

    feat: 메뉴가 여러개일때 navigate 버튼 추가 기능 구현

--- src/app/(post)/posts/[postId]/page.tsx
+++ src/app/(post)/posts/[postId]/page.tsx
@@ -7,6 +7,7 @@ import { apiService } from '@/services/apiService';
 import { getCookieValue } from '@/utils';
 
 import s from './page.module.scss';
+import { CategoriesEN } from '@/app/page';
 
 interface Props {
   params: {
@@ -14,27 +15,49 @@ interface Props {
   };
 }
 
-interface PostDetails {
-  id: string;
-  writer: string;
-  createDate: string;
-  postContent: string;
-  postImage: string;
+interface PostDetailsResponse {
+  postId: number;
+  memberUsername: string;
+  storeName: string;
+  storeLocation: {
+    address: string;
+    posx: string;
+    posy: string;
+  };
   postMainMenu: string;
-  title: string;
-  content: string;
+  postCategory: Exclude<CategoriesEN, 'ALL'>;
+  permission: 'PRIVATE';
+  menus: MenuResponse[];
+  auto: boolean;
+  finished: boolean;
+}
+
+interface MenuResponse {
+  menuId: string;
+  menuImage: {
+    id: string;
+    url: string;
+  };
+  menuName: string;
+  menuPrice: string;
+  menuContent: string;
 }
 
 export default function PostPage({ params: { postId } }: Props) {
-  const [postDetails, setPostDetails] = useState<PostDetails | null>(null);
+  const [postDetails, setPostDetails] = useState<PostDetailsResponse | null>(
+    null,
+  );
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [currentMenuIndex, setCurrentMenuIndex] = useState(0); // 추가된 상태
+
   const usernameFromCookie = getCookieValue('username');
-  const postModdable = postDetails?.writer === usernameFromCookie;
+  const postModdable = postDetails?.memberUsername === usernameFromCookie;
 
   useEffect(() => {
     apiService
       .fetchPost(postId)
       .then((data) => {
+        console.log(data);
         setPostDetails(data);
       })
       .catch((e) => {
@@ -42,7 +65,17 @@ export default function PostPage({ params: { postId } }: Props) {
       });
   }, [postId]);
 
-  const { id, postContent, postMainMenu, writer } = postDetails || {};
+  // 메뉴 변경 함수 추가
+  const goToNextMenu = () => {
+    if (postDetails && postDetails.menus.length > currentMenuIndex + 1) {
+      setCurrentMenuIndex(currentMenuIndex + 1);
+    }
+  };
+
+  const menu = postDetails?.menus[currentMenuIndex]; // 현재 메뉴 정보
+  const { address } = postDetails?.storeLocation || {};
+
+  console.log(menu?.menuImage.url);
 
   return (
     <div>
@@ -50,18 +83,25 @@ export default function PostPage({ params: { postId } }: Props) {
       {errorMsg && <div>{errorMsg}</div>}
       {postDetails && (
         <div>
-          <div className={s.container} data-id={id}>
+          <div className={s.container} data-id={postDetails.postId}>
             <ImageFill
-              src="/images/mock-food.png"
-              alt={postMainMenu || 'food-image'}
+              src={menu?.menuImage.url || '/images/mock-food.png'}
+              alt={menu?.menuName || 'food-image'}
               fill
               height="246px"
             />
-            <div>울엄 김치찜 광교역점</div>
-            <div>서울 역삼 어딘가</div>
-            <div>작성자 : {writer}</div>
-            <div>12,000원</div>
-            <div>{postContent}</div>
+            <div>{postDetails.storeName}</div>
+            <div>{address}</div>
+            <div>작성자 : {postDetails.memberUsername}</div>
+            <div>{menu?.menuName}</div>
+            <div>{menu?.menuPrice}</div>
+            <div>{menu?.menuContent}</div>
+            {postDetails.menus.length > 1 &&
+              currentMenuIndex < postDetails.menus.length - 1 && (
+                <button type="button" onClick={goToNextMenu}>
+                  다음 메뉴 보기
+                </button>
+              )}
           </div>
         </div>
       )}
