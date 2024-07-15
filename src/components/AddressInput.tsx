41482ec918f0772278f2eb91f1235617a6644f49
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-07-15 17:39:03 +0900

    feat: 주소 검색 input component 분리

--- src/components/AddressInput.tsx
+++ src/components/AddressInput.tsx
@@ -1,26 +1,42 @@
-import React, {
-  useState,
-  ChangeEvent,
-  useRef,
-  useEffect,
-  FormEvent,
-} from 'react';
+'use client';
+
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable camelcase */ // for KAKAO API response
+
+import React, { useState, useRef, useEffect } from 'react';
+import Image from 'next/image';
 
 import { apiService } from '@/services/apiService';
-import Button from '@/components/ui/Button';
+import InputBox from '@/components/ui/InputBox';
 
 import { AddressSearchDetails } from '@/app/(post)/write/page';
 
 import s from './AddressInput.module.scss';
 
 interface AddressInputProps {
+  title: string;
   value: string;
   onSelect: (addressSearchResult: AddressSearchDetails) => void;
 }
 
-export default function AddressInput({ value, onSelect }: AddressInputProps) {
+interface KAKAOSearchAddressResponse {
+  id: string;
+  address_name: string;
+  place_name: string;
+  x: string;
+  y: string;
+  place_url: string;
+}
+
+export default function AddressInput({
+  title,
+  value,
+  onSelect,
+}: AddressInputProps) {
   const [query, setQuery] = useState(value);
-  const [results, setResults] = useState<string[]>([]);
+  const [results, setResults] = useState<KAKAOSearchAddressResponse[]>([]);
   const [isFocused, setIsFocused] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -43,40 +59,29 @@ export default function AddressInput({ value, onSelect }: AddressInputProps) {
 
     const handleKeyDown = (e: KeyboardEvent) => {
       e.stopPropagation();
-
-      // if (e.key === 'Enter') {
-      //   e.preventDefault();
-      // }
     };
 
     document.addEventListener('mousedown', handleClickOutside);
-    document.addEventListener('keydown', handleKeyDown, { passive: false });
-    document.addEventListener('drag', preventDefault, { passive: false });
+    document.addEventListener('keydown', handleKeyDown);
+    if (isFocused) {
+      document.addEventListener('drag', preventDefault);
+    }
 
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
-      document.removeEventListener('keydown', handleKeyDown, {
-        passive: false,
-      });
-      document.removeEventListener('drag', preventDefault, { passive: false });
+      document.removeEventListener('keydown', handleKeyDown);
+      if (isFocused) {
+        document.removeEventListener('drag', preventDefault);
+      }
     };
   }, [inputRef, isFocused]);
 
-  const handleBlurBackgroundClick = () => {
-    setIsFocused(false);
-  };
+  const fetchKakaoAddress = async (
+    query: string,
+  ): Promise<KAKAOSearchAddressResponse[]> => {
+    const res = await apiService.fetchKAKAOStoreInfo(query);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const fetchedResults = await fetchKakaoAddress(query);
-    setResults(fetchedResults);
-  };
-
-  const fetchKakaoAddress = async (query: string): Promise<string[]> => {
-    const response = await apiService.fetchKAKAOStoreInfo(query);
-    console.log(response);
-    // Assuming `data` is defined and contains the necessary information
-    return response.documents;
+    return res.documents;
   };
 
   const handleSelect = (addressSearchDetails: AddressSearchDetails) => {
@@ -85,35 +90,30 @@ export default function AddressInput({ value, onSelect }: AddressInputProps) {
     setIsFocused(false);
   };
 
-  const handleButtonClick = async () => {
+  const handleSearchClick = async () => {
     const fetchedResults = await fetchKakaoAddress(query);
 
-    console.log(fetchedResults);
     setResults(fetchedResults);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      handleButtonClick();
-    }
+    if (e.key === 'Enter') handleSearchClick();
   };
 
   return (
     <div className={`${s.container} ${isFocused ? s.blur : ''}`}>
       {isFocused && (
-        <div
-          className={s.blurBackground}
-          onClick={handleBlurBackgroundClick}
-          ref={inputRef}
-        >
+        <div ref={inputRef}>
           <div
             className={`${s.addressInputWrapper} ${isFocused ? s.expanded : ''}`}
             onClick={(e) => e.stopPropagation()}
           >
             <div className={s.searchArea}>
               <input
+                className={s.addressInput}
                 id="address"
                 name="address"
+                placeholder="ex) 스타벅스, 이디야"
                 type="text"
                 value={query}
                 onFocus={() => setIsFocused(true)}
@@ -121,26 +121,42 @@ export default function AddressInput({ value, onSelect }: AddressInputProps) {
                 onKeyDown={handleKeyDown}
                 readOnly={!isFocused}
               />
-              <Button text="검색" type="button" onClick={handleButtonClick} />
+              {/* <Button text="검색" type="button" onClick={handleButtonClick} /> */}
+              <button
+                className={s.searchBtn}
+                type="button"
+                onClick={handleSearchClick}
+              >
+                <Image
+                  src="/images/search-glass.svg"
+                  alt="검색"
+                  width={20}
+                  height={20}
+                />
+              </button>
             </div>
             <ul className={s.results} onWheel={(e) => e.stopPropagation()}>
               {results.map(
-                ({ id, address_name, place_name, x, y, place_url }) => (
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                ({ id, address_name, place_name, x, y }) => (
                   <li
                     key={id}
-                    onClick={() =>
+                    onClick={() => {
                       handleSelect({
                         address: address_name,
                         storeName: place_name,
                         posx: x,
                         posy: y,
-                      })
-                    }
+                      });
+                    }}
                   >
-                    <p>{address_name}</p>
-                    <p>{place_name}</p>
-                    <p>{x}</p>
-                    <p>{y}</p>
+                    <div className={s.searchResult}>
+                      <p className={s.storeName}>{place_name}</p>
+                      <div className={s.loadNameArea}>
+                        <span className={s.badge}>도로명</span>
+                        <p className={s.loadName}>{address_name}</p>
+                      </div>
+                    </div>
                   </li>
                 ),
               )}
@@ -149,15 +165,19 @@ export default function AddressInput({ value, onSelect }: AddressInputProps) {
         </div>
       )}
       {!isFocused && (
-        <input
-          id="address"
-          name="address"
-          type="text"
-          value={query}
-          onFocus={() => setIsFocused(true)}
-          onChange={(e) => setQuery(e.target.value)}
-          readOnly={!isFocused}
-        />
+        <div>
+          <InputBox
+            height="30px"
+            label={title}
+            placeholder="가게 이름을 입력하면 주소가 자동완성됩니다."
+            name="address"
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onFocus={() => setIsFocused(true)}
+            readOnly={!isFocused}
+          />
+        </div>
       )}
     </div>
   );
