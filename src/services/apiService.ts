423dcbf45608133c0bb5a8145354a4dae1e4cba0
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-07-15 17:53:46 +0900

    feat: build를 안했을시에 msw 서버 사용하도록 baseUrl 변경

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,11 +1,12 @@
 import { LoginUserInputs, SignupDetails } from '@/types';
 
 class ApiService {
-  private baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-
-  // private baseUrl = 'http://localhost:9090';
-
   private mswBaseUrl = 'http://localhost:9090';
+
+  private baseUrl =
+    process.env.NODE_ENV === 'production'
+      ? process.env.NEXT_PUBLIC_BASE_URL
+      : this.mswBaseUrl;
 
   private kakaoBaseUrl =
     'https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=15&sort=accuracy&query=';
