30bea72f2c16123748b17fc030c681e55e19917e
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-07-20 23:35:43 +0900

    feat: 라이브 스트리밍 page 기능 정상작동 확인

--- src/app/live/UserVideoComponent.jsx
+++ src/app/live/UserVideoComponent.jsx
@@ -3,20 +3,10 @@ import OpenViduVideoComponent from './OvVideo';
 import './UserVideo.css';
 
 function UserVideoComponent({ streamManager }) {
-  const getNicknameTag = () => {
-    // Assuming `streamManager` and `stream` are properly checked for null/undefined elsewhere in the application
-    return JSON.parse(streamManager.stream.connection.data).clientData;
-  };
-
   return (
     <div>
       {streamManager ? (
-        <div className="streamcomponent">
-          <OpenViduVideoComponent streamManager={streamManager} />
-          <div>
-            <p>{getNicknameTag()}</p>
-          </div>
-        </div>
+        <OpenViduVideoComponent streamManager={streamManager} />
       ) : null}
     </div>
   );
--- src/app/live/layout.tsx
+++ src/app/live/layout.tsx
@@ -1,24 +1,10 @@
+// import LiveHeader from '@/app/live/_components/LiveHeader';
 import { ReactNode } from 'react';
 
 export default function LiveLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <header
-        style={{
-          position: 'sticky',
-          top: 0,
-          left: 0,
-          padding: '0 24px',
-          height: '60px',
-          backgroundColor: 'lightblue',
-
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <div>back</div>
-      </header>
+      {/* <LiveHeader /> */}
       {children}
     </div>
   );
