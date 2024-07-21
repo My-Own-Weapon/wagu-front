99452968a7896be432da5927ac521b4c09e09dea
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-07-21 20:59:34 +0900

    feat[MVP]: live방 생성 page mvp 구현

--- src/app/live/layout.tsx
+++ src/app/live/layout.tsx
@@ -1,11 +1,8 @@
+'use client';
+
 // import LiveHeader from '@/app/live/_components/LiveHeader';
 import { ReactNode } from 'react';
 
 export default function LiveLayout({ children }: { children: ReactNode }) {
-  return (
-    <div>
-      {/* <LiveHeader /> */}
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
