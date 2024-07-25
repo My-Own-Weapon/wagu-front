0e22d64d6b1474b51c2e9baf5418745c3b6e1801
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-07-25 09:23:56 +0900

    refactor: 프로필 상세페이지 새로운 디자인 시안으로 변경

--- public/newDesign/nav/index.ts
+++ public/newDesign/nav/index.ts
@@ -3,3 +3,5 @@ export { default as TropySVG } from './tropy.svg';
 export { default as LiveSVG } from './live.svg';
 export { default as MapPinSVG } from './map_pin.svg';
 export { default as PlusSVG } from './plus.svg';
+export { default as BtnBackSVG } from './btn_back.svg';
+export { default as DotDotDotSVG } from './dot-dot-dot.svg';
