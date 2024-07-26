c213df814e4e4c39dd8b691115f4e06eea35c721
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-07-26 15:15:42 +0900

    style: ai 리뷰 자동완성 버튼 스타일 수정

--- public/newDesign/index.ts
+++ public/newDesign/index.ts
@@ -1,3 +1,4 @@
 export { default as CameraSVG } from './camera.svg';
 export { default as CaretDownSVG } from './caret_down.svg';
 export { default as XSVG } from './X.svg';
+export { default as StarAISVG } from './star_ai.svg';
