80078fffb8e0e5aafb2456463e58f3d8c4bbc36b
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-09-14 19:00:35 +0900

    chore: 미사용 module 삭제

--- src/components/ui/TextareaBox/__tests__/index.test.tsx
+++ src/components/ui/TextareaBox/__tests__/index.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { TextareaBox } from '@/components/ui';
 
