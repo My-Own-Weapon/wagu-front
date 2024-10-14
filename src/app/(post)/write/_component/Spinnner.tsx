101f98c4d333adb2215920780e0659dc9435f457
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-10-14 22:24:31 +0900

    rename: 미사용 컴포넌트 삭제

--- src/app/(post)/write/_component/Spinnner.tsx
+++
@@ -1,9 +0,0 @@
-import s from './Spinner.module.scss';
-
-export default function Spinner() {
-  return (
-    <div className={s.spinnerOverlay}>
-      <div className={s.spinner} />
-    </div>
-  );
-}
