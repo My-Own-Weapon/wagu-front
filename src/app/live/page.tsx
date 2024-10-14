0c1f5807ee0a86267df842e7520ea1c946c35742
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-10-15 06:09:57 +0900

    style: 라이브 방송 준비 페이지 스타일 1차 수정

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,27 +1,19 @@
 'use client';
 
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable no-shadow */
-/* eslint-disable react/jsx-filename-extension */
-/* eslint-disable max-depth */
-
 import React, { FormEventHandler, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import { AddressSearchDetails } from '@/types';
 import { apiService } from '@services/apiService';
 import AddressInput from '@/components/AddressInput';
-import Header from '@/components/Header';
-
-import s from './page.module.scss';
+import { BoxButton, Container } from '@/components/ui';
+import Camera from '@/app/live/prepare/Camera';
 
 export default function PrepareStreamingPage() {
   const [storeInfo, setStoreInfo] = useState<AddressSearchDetails | null>(null);
   const router = useRouter();
 
-  const joinSession: FormEventHandler = async (e) => {
+  const handleClickLiveStart: FormEventHandler = async (e) => {
     e.preventDefault();
     if (!storeInfo) {
       alert('가게 정보를 입력해주세요');
@@ -30,34 +22,22 @@ export default function PrepareStreamingPage() {
 
     const { sessionId } = await apiService.createSessionId(storeInfo);
 
-    alert(sessionId);
+    alert('라이브 방송을 시작합니다.');
 
     router.push(`/live/${sessionId}`);
   };
 
   return (
-    <>
-      <Header />
-      <div
-        className={s.entryPageContainer}
-        style={{
-          marginTop: '100px',
-        }}
-      >
-        <div className={s.entryPageHeaderWrapper}>
-          <h1 className={s.title}> 스트리밍 방 참가 페이지 </h1>
-          <AddressInput
-            title="가게 이름"
-            value={
-              storeInfo ? `[${storeInfo?.storeName}] ${storeInfo?.address}` : ''
-            }
-            onSelect={setStoreInfo}
-          />
-          <form className={s.joinInfoWrapper} onSubmit={joinSession}>
-            <input name="commit" type="submit" value="세션 참여 !!" />
-          </form>
-        </div>
-      </div>
-    </>
+    <Container>
+      <Camera />
+      <AddressInput
+        title="가게 이름"
+        value={
+          storeInfo ? `[${storeInfo?.storeName}] ${storeInfo?.address}` : ''
+        }
+        onSelect={setStoreInfo}
+      />
+      <BoxButton onClick={handleClickLiveStart}>라이브 시작</BoxButton>
+    </Container>
   );
 }
