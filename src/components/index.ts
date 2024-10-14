dc87dfadafd6685b06e69c1bfac76d82a3fa88cc
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-10-14 23:30:39 +0900

    chore: 모듈화를 위한 파일

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import AddressInput from '@/components/AddressInput';
+import PostCard from '@/components/PostCard';
+import WithStyles from '@/components/WithStyles';
+import BackBtn from '@/components/BackBtn';
+
+export { PostCard, WithStyles, AddressInput, BackBtn };
