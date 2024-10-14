4d0c4bf2cd62aef09fb28ab516e5b2d719af4245
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-10-14 23:23:27 +0900

    refactor(Write Page): 리뷰 작성 페이지에서만 사용되는 훅 모듈화

---
+++ src/app/(post)/write/_hooks/index.ts
@@ -0,0 +1,13 @@
+import useFetchAIAutoReview from '@/app/(post)/write/_hooks/useFetchAiReview';
+import useMenuForm from '@/app/(post)/write/_hooks/useMenuForm';
+import usePageIndexControl from '@/app/(post)/write/_hooks/usePageIndexControl';
+import useReviewInputsSection from '@/app/(post)/write/_hooks/useReviewInputsSection';
+import useWritePageFormControl from '@/app/(post)/write/_hooks/useWritePageFormControl';
+
+export {
+  useFetchAIAutoReview,
+  useMenuForm,
+  usePageIndexControl,
+  useReviewInputsSection,
+  useWritePageFormControl,
+};
