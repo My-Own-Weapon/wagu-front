a41f865f90cccd6772e6766b922dc6ee49c8d8d7
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-10-15 21:27:55 +0900

    chore: stylelint-config-recess-order/groups관련 버그 주석 추가

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,3 +1,6 @@
+/**
+ * @warning propertyGroups는 ESM 모듈이므로 CommonJS에서 가져올 시에 require가 아닌 import를 사용해야 합니다.
+ */
 import propertyGroups from 'stylelint-config-recess-order/groups';
 
 export default {
