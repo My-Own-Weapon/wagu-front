548c7a77b06d69a5eba969cf50453d258cec3e06
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-10-25 12:35:07 +0900

    test: 리뷰 작성 실패 시나리오 테스트 추가

--- tests/writePage.spec.ts
+++ tests/writePage.spec.ts
@@ -1,28 +1,25 @@
 import test, { expect } from '@playwright/test';
 import { baseUrl } from './constants';
 import WritePageModel from './pageModel/WritePageModel';
+import { delay } from '@/utils';
 
 test.describe('글쓰기 page 테스트', () => {
-  // let writePageModel: WritePageModel;
+  let I: WritePageModel;
 
-  // test.beforeEach(async ({ page }) => {
-  //   await page.goto(`${baseUrl}/write`);
-  //   writePageModel = new WritePageModel(page);
-  // });
+  test.beforeEach(async ({ page }) => {
+    I = new WritePageModel(page);
+  });
 
-  test('리뷰 작성 시나리오', async ({ page }) => {
-    const I = new WritePageModel(page);
-
+  test('리뷰 작성 성공 시나리오', async ({ page }) => {
     // 1. 페이지로 이동
     await I.gotoWritePage();
 
     // 2. 식당 정보 입력 (AddressInput 컴포넌트)
-    await page.click('input[name="address"]');
-    await page.fill('input[name="address"]', '스타벅스');
+    await I.addressSearchTrigger.click();
+    await I.addressInput.fill('스타벅스');
     await page.keyboard.press('Enter');
-    const addressSearchResults = page.locator(
-      'ul[data-testId="address-search-results"]',
-    );
+
+    const addressSearchResults = page.getByTestId('address-search-results');
     await expect(addressSearchResults).toBeVisible();
     await addressSearchResults.first().click();
     await expect(addressSearchResults).not.toBeVisible();
@@ -40,21 +37,59 @@ test.describe('글쓰기 page 테스트', () => {
     await page.setInputFiles('input[type="file"]', filePath);
 
     // 5. 대표 메뉴 입력
-    await page.fill('input[name="menuReviews.0.menuName"]', '아메리카노');
+    await I.menuNameInput.fill('아메리카노');
 
     // 6. 가격 입력
-    await page.fill('input[name="menuReviews.0.menuPrice"]', '5000');
+    await I.menuPriceInput.fill('5000');
 
     // 7. 리뷰 입력
-    await page.fill(
-      'textarea[name="menuReviews.0.menuContent"]',
-      'JWT 아 JMT인가 ㅎ.',
-    );
+    await I.reviewInput.fill('JWT 아 JMT인가 ㅎ.');
 
     // 8. 포스트 등록 버튼 클릭
-    await page.click('button[type="submit"]'); // 포스트 등록 버튼 클릭
+    await I.submitButton.click();
 
     // 9. 등록 완료 확인 (API 응답 확인)
     await expect(page).toHaveURL(`${baseUrl}/`);
+  });
+
+  test('리뷰 작성 실패 시나리오 (식당 주소를 입력하지 않은 경우)', async ({
+    page,
+  }) => {
+    // 1. 페이지로 이동
+    await I.gotoWritePage();
+
+    // 2. 식당 정보를 입력하지 않습니다.
+
+    // 3. 카테고리 선택 (CategorySelect 컴포넌트)
+    await I.categorySelectTrigger.click();
+    await page.getByText('카페').click();
+
+    // 4. 이미지 추가 (ImageInput 컴포넌트)
+    const filePath = 'public/images/mock-food.png';
+    await page.setInputFiles('input[type="file"]', filePath);
+
+    // 5. 대표 메뉴 입력
+    await I.menuNameInput.fill('아메리카노');
+
+    // 6. 가격 입력
+    await I.menuPriceInput.fill('5000');
+
+    // 7. 리뷰 입력
+    await I.reviewInput.fill('JWT 아 JMT인가 ㅎ.');
+
+    // 8. 포스트 등록 버튼 클릭 및 dialog 대기
+    const [dialog] = await Promise.all([
+      page.waitForEvent('dialog'),
+      I.submitButton.click(),
+    ]);
+
+    // dialog 메시지 확인
+    expect(dialog.message()).toContain('리뷰 입력란을 모두 채워주세요');
+
+    // dialog 닫기
+    await dialog.accept();
+
+    // 페이지는 이동하지 않습니다.
+    await expect(page).toHaveURL(`${baseUrl}/write`);
   });
 });
