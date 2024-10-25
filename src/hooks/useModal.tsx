a21f423338246d88070af11fb09a532fdf1b590f
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-10-25 11:25:40 +0900

    refactor: 기본 overlay를 지정하지 않도록 변경

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, ReactNode, useCallback, useReducer } from 'react';
+import { ReactNode, useCallback, useReducer } from 'react';
 import { createPortal } from 'react-dom';
 
 interface ModalWrapper {
@@ -12,53 +12,8 @@ export default function useModal() {
   const close = useCallback(() => toggleIsOpen(), []);
 
   function ModalCompnent({ content }: ModalWrapper) {
-    return (
-      isOpen &&
-      createPortal(<Overlay close={close}>{content}</Overlay>, document.body)
-    );
+    return isOpen && createPortal(content, document.body);
   }
 
-  return { open, close, ModalCompnent };
-}
-
-const STYLE = {
-  OVERLAY: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    bottom: 0,
-
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    backdropFilter: 'blur(2px)',
-    zIndex: 9999,
-  },
-} as const;
-
-interface OverlayProps {
-  close: () => void;
-  children: ReactNode;
-}
-
-function Overlay({ close, children }: PropsWithChildren<OverlayProps>) {
-  return (
-    <div
-      style={{ ...STYLE.OVERLAY }}
-      onClick={close}
-      onKeyDown={(e) => {
-        e.stopPropagation();
-
-        if (e.key === 'backspace') {
-          close();
-        }
-      }}
-      role="button"
-      tabIndex={0}
-    >
-      {children}
-    </div>
-  );
+  return { open, isOpen, close, ModalCompnent };
 }
