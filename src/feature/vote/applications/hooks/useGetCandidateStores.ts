c59eeee39a936ef0da49ccac063d7335dbc3b622
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-11-22 18:39:50 +0900

    refactor: selector를 넘기지않았을때 자동으로 추론되도록 수정

--- src/feature/vote/applications/hooks/useGetCandidateStores.ts
+++ src/feature/vote/applications/hooks/useGetCandidateStores.ts
@@ -1,24 +1,24 @@
 import CandidateStoresViewModel, {
   CandidateStoreViewModelProps,
-} from '@/feature/vote/applications/viewModels/CandidateStoresViewModel';
-import { voteApiService } from '@/feature/vote/services/api/voteApiService';
+} from '@/feature/vote/viewModels/CandidateStoresViewModel';
 import { useFetchCandidateStores } from '@/feature/vote/services/hooks';
 
 type RTCSessionId = string;
 
 const useGetCandidateStores = (sessionId: RTCSessionId) => {
-  const { data: candidateStores } =
-    useFetchCandidateStores<CandidateStoreViewModelProps>({
-      sessionId,
-      selector: (serverData) => {
-        return serverData.map((store) => ({
-          mainMenuName: store.menuName,
-          mainMenuImageUrl: store.menuImage.url,
-          storeName: store.storeName,
-          storeId: store.storeId,
-        }));
-      },
-    });
+  const { data: candidateStores } = useFetchCandidateStores<
+    CandidateStoreViewModelProps[]
+  >({
+    sessionId,
+    selector: (serverData) => {
+      return serverData.map((store) => ({
+        mainMenuName: store.menuName,
+        mainMenuImageUrl: store.menuImage.url,
+        storeName: store.storeName,
+        storeId: store.storeId,
+      }));
+    },
+  });
 
   const candidateStoresViewModels = candidateStores.map((store) => {
     return new CandidateStoresViewModel(store);
--- src/feature/vote/services/hooks/useFetchCandidateStores.ts
+++ src/feature/vote/services/hooks/useFetchCandidateStores.ts
@@ -1,46 +1,30 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { voteApiService } from '@/feature/vote/services/api/voteApiService';
-import { OriginalServerData, RTCSessionId, Selector } from '@/feature/_types';
+import {
+  OriginalServerData,
+  RTCSessionId,
+  WithSelector,
+} from '@/feature/_types';
 
 type ServerData = OriginalServerData<
   typeof voteApiService.fetchCandidateStores
 >;
 
-const useFetchCandidateStores = <T = DefaultItem>({
-  sessionId,
-  selector = defaultSelector as Selector<ServerData, T>,
-}: {
+interface Props<T> extends WithSelector<ServerData, T> {
   sessionId: RTCSessionId;
-  selector?: Selector<ServerData, T>;
-}) => {
-  const query = useSuspenseQuery<ServerData, Error, T[]>({
+}
+
+const useFetchCandidateStores = <T = ServerData>({
+  sessionId,
+  selector,
+}: Props<T>) => {
+  const query = useSuspenseQuery({
     queryKey: ['fetchCandidateStores', sessionId],
     queryFn: () => voteApiService.fetchCandidateStores(sessionId),
     select: selector,
   });
 
   return query;
-};
-
-type DefaultItem = {
-  mainMenuName: string;
-  mainMenuImageUrl: string;
-  mainMenuImageId: number;
-  storeName: string;
-  storeId: number;
-  storePostCount: number;
-};
-const defaultSelector = (data: ServerData): DefaultItem[] => {
-  return data.map((store) => {
-    return {
-      mainMenuName: store.menuName,
-      mainMenuImageUrl: store.menuImage.url,
-      mainMenuImageId: store.menuImage.id,
-      storeName: store.storeName,
-      storeId: store.storeId,
-      storePostCount: store.postCount,
-    };
-  });
 };
 
 export default useFetchCandidateStores;
