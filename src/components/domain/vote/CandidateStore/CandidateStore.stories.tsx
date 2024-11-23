667b7a65e0ae040b398a7dae5f9cd32990d1726b
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-11-23 22:13:40 +0900

    feat: 투표 후보자로 등록된 store 컴포넌트

--- src/components/domain/vote/CandidateStore/CandidateStore.stories.tsx
+++ src/components/domain/vote/CandidateStore/CandidateStore.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import CandidateStore from './CandidateStore';
-import { CandidateStoresViewModel } from '@/feature/vote/applications/viewModels';
+import { CandidateStoresViewModel } from '@/feature/vote/viewModels';
+import { CandidateStore } from '@/components/domain/vote';
+import { Spacing } from '@/components/ui';
+import { Fragment } from 'react';
 
 const meta = {
   title: 'Domain/Vote/CandidateStore',
@@ -11,7 +13,7 @@ const meta = {
   tags: ['autodocs'],
   decorators: [
     (Story) => (
-      <div style={{ width: '410px' }}>
+      <div style={{ width: '410px', padding: '16px' }}>
         <Story />
       </div>
     ),
@@ -48,5 +50,37 @@ export const LongStoreName: Story = {
     onRemoveCandidateStore: () => {
       alert('가게가 투표 목록에서 제거되었습니다.');
     },
+  },
+};
+
+export const candidateStoreList: Story = {
+  args: {
+    viewModel: mockViewModel,
+    onRemoveCandidateStore: () => {
+      alert('가게가 투표 목록에서 제거되었습니다.');
+    },
+  },
+
+  render: (args) => {
+    return (
+      <ul>
+        {Array.from({ length: 5 }).map((_, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Fragment key={index}>
+            <div
+              style={{
+                position: 'relative',
+                border: '1px solid tomato',
+                borderRadius: '4px',
+                paddingLeft: '16px',
+              }}
+            >
+              <CandidateStore {...args} />
+            </div>
+            {index !== 4 && <Spacing size={16} />}
+          </Fragment>
+        ))}
+      </ul>
+    );
   },
 };
--- src/components/domain/vote/CandidateStore/CandidateStore.tsx
+++ src/components/domain/vote/CandidateStore/CandidateStore.tsx
@@ -1,33 +1,51 @@
 import { MouseEventHandler } from 'react';
-import { CandidateStoresViewModel } from '@/feature/vote/viewModels';
 import { UserIcon } from '@/components/UserIcon';
+import ViewModel from '@/feature/_lib/ViewModel';
+import { Flex, Text } from '@/components/ui';
+import { colors } from '@/constants/theme';
 
 import s from './CandidateStore.module.scss';
+
+export interface CandidateStoresViewModelImpl {
+  getStoreName: () => string;
+  getStoreId: () => number;
+}
 
 export default function CandidateStore({
   viewModel,
   onRemoveCandidateStore,
 }: {
-  viewModel: CandidateStoresViewModel;
+  viewModel: ViewModel & CandidateStoresViewModelImpl;
   onRemoveCandidateStore: MouseEventHandler<HTMLButtonElement>;
 }) {
   return (
-    <li className={s.votedStoreCardWrapper}>
-      <p className={s.storeName}>{viewModel.getStoreName()}</p>
-      <UserIcon
-        size="small"
-        shape="circle"
-        imgSrc="/profile/profile-default-icon-male.svg"
-        alt="profile-img"
-      />
-      <button
-        className={s.removeBtn}
-        type="button"
-        data-store-id={viewModel.getStoreId()}
-        onClick={onRemoveCandidateStore}
+    <li aria-label="후보 가게 카드">
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+        style={{
+          width: '100%',
+          height: '56px',
+          position: 'relative',
+        }}
       >
-        x
-      </button>
+        <Text color={colors.grayAsh800} fontWeight="medium" fontSize="medium">
+          {viewModel.getStoreName()}
+        </Text>
+        <UserIcon
+          size="small"
+          shape="circle"
+          imgSrc="/profile/profile-default-icon-male.svg"
+          alt="profile-img"
+        />
+        <button
+          data-testid="remove-candidate-store-button"
+          className={s.removeBtn}
+          type="button"
+          data-store-id={viewModel.getStoreId()}
+          onClick={onRemoveCandidateStore}
+        />
+      </Flex>
     </li>
   );
 }
