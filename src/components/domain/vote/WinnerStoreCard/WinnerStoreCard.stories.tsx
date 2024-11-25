1fbd1e2e34ae6f539fb4db343e1c7fbc7d3d65a2
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-11-26 06:52:22 +0900

    [refactor] 우승자 가게 카드 컴포넌트 분리

--- src/components/domain/vote/WinnerStoreCard/WinnerStoreCard.stories.tsx
+++ src/components/domain/vote/WinnerStoreCard/WinnerStoreCard.stories.tsx
@@ -1,45 +1,85 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import WinStoreCard from './WinnerStoreCard';
+
+import { VoteWinnerStoreViewModel } from '@/feature/vote/viewModels';
+import WinnerStoreCard from './WinnerStoreCard';
+
+interface StoryArgs {
+  viewModel?: VoteWinnerStoreViewModel;
+  storeName: string;
+  mainMenuName: string;
+}
 
 const meta = {
   title: 'Domain/Vote/WinnerStoreCard',
-  component: WinStoreCard,
+  component: WinnerStoreCard,
   parameters: {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  args: {
+    storeName: '평범한 이름의 식당',
+    mainMenuName: '평범한 메뉴',
+    viewModel: undefined,
+  },
+  argTypes: {
+    storeName: {
+      description: '가게 상호명',
+      control: 'text',
+    },
+    mainMenuName: {
+      description: '해당 가게의 메인 메뉴 이름',
+      control: 'text',
+    },
+    viewModel: {
+      description: 'WinnerStoreCard 컴포넌트에 전달되는 뷰모델',
+      control: false,
+    },
+  },
   decorators: [
     (Story) => (
-      <div style={{ width: '410px' }}>
+      <div style={{ width: '375px' }}>
         <Story />
       </div>
     ),
   ],
-} satisfies Meta<typeof WinStoreCard>;
+} satisfies Meta<typeof WinnerStoreCard | StoryArgs>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    storeId: 1,
-    storeName: '맛있는 식당',
-    menuImage: {
-      url: 'https://picsum.photos/400/300',
-    },
-    postCount: 10,
-    menuName: '시그니처 메뉴',
+    storeName: '평범한 이름의 식당',
+    mainMenuName: '평범한 메뉴',
+    viewModel: undefined,
+  },
+  render: (args) => {
+    const viewModel = new VoteWinnerStoreViewModel({
+      storeId: 1,
+      storeName: args.storeName,
+      mainMenuImageUrl: 'https://picsum.photos/400/300',
+      mainMenuName: args.mainMenuName,
+      storePostCount: 10,
+    });
+
+    return <WinnerStoreCard viewModel={viewModel} />;
   },
 };
 
 export const LongText: Story = {
   args: {
-    storeId: 2,
     storeName: '매우 긴 가게 이름을 가진 맛있는 식당',
-    menuImage: {
-      url: 'https://picsum.photos/400/300',
-    },
-    postCount: 15,
-    menuName: '매우 긴 메뉴 이름을 가진 시그니처 메뉴',
+    mainMenuName: '매우 긴 메뉴 이름을 가진 시그니처 메뉴',
+  },
+  render: (args) => {
+    const viewModel = new VoteWinnerStoreViewModel({
+      storeId: 2,
+      mainMenuImageUrl: 'https://picsum.photos/400/300',
+      storeName: args.storeName,
+      mainMenuName: args.mainMenuName,
+      storePostCount: 10,
+    });
+
+    return <WinnerStoreCard viewModel={viewModel} />;
   },
 };
--- src/components/domain/vote/WinnerStoreCard/WinnerStoreCard.tsx
+++ src/components/domain/vote/WinnerStoreCard/WinnerStoreCard.tsx
@@ -2,31 +2,20 @@ import { NextImageWithCover, Spacing, Stack, Text } from '@/components/ui';
 import Link from 'next/link';
 
 import { colors } from '@/constants/theme';
+import ViewModel from '@/feature/_lib/ViewModel';
+import { WinnerStoreViewModelImpl } from './WinnerStoreCardImple';
 
-interface Props {
-  storeId: number;
-  storeName: string;
-  menuImage: {
-    url: string;
-  };
-  postCount: number;
-  menuName: string;
+interface WinnerStoreCardProps {
+  viewModel: ViewModel & WinnerStoreViewModelImpl;
 }
-export default function WinStoreCard({
-  storeId,
-  storeName,
-  menuImage,
-  postCount,
-  menuName,
-}: Props) {
-  const { url } = menuImage;
 
+export default function WinnerStoreCard({ viewModel }: WinnerStoreCardProps) {
   return (
-    <Link style={{ width: '100%' }} href={`/store/${storeId}`}>
+    <Link style={{ width: '100%' }} href={`/store/${viewModel.getStoreId()}`}>
       <Stack>
         <NextImageWithCover
-          src={url}
-          id={String(storeId)}
+          src={viewModel.getMainMenuImageUrl()}
+          id={String(viewModel.getStoreId())}
           alt="vote-win-store-img"
           height="280px"
           borderRadius="24px"
@@ -34,11 +23,11 @@ export default function WinStoreCard({
         <Spacing size={16} />
         <Stack>
           <Text fontWeight="bold" fontSize="large" color={colors.grayAsh900}>
-            {storeName}
+            {viewModel.getStoreName()}
           </Text>
           <Spacing size={8} />
           <Text fontWeight="medium" fontSize="medium" color={colors.grayAsh800}>
-            {menuName}
+            {viewModel.getMainMenuName()}
           </Text>
         </Stack>
       </Stack>
