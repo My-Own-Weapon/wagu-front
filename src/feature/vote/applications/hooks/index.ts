33f03fb66fccaaba648bb026b0dfcf38fc010006
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2024-11-26 06:51:01 +0900

    [refactor] 투표 후보자 관련 GET, POST, DELETE 메서드 분리

--- src/feature/vote/applications/hooks/index.ts
+++ src/feature/vote/applications/hooks/index.ts
@@ -1,1 +1,4 @@
 export { default as useGetCandidateStores } from './useGetCandidateStores';
+export { default as useGetWinnerStores } from './useGetWinnerStores';
+export { default as useRemoveCandidateStore } from './useRemoveCandidateStore';
+export { default as useAddCandidateStore } from './useAddCandidateStore';
--- src/feature/vote/applications/hooks/useGetCandidateStores.ts
+++ src/feature/vote/applications/hooks/useGetCandidateStores.ts
@@ -1,24 +1,23 @@
-import CandidateStoresViewModel, {
-  CandidateStoreViewModelProps,
-} from '@/feature/vote/viewModels/CandidateStoresViewModel';
-import { useFetchCandidateStores } from '@/feature/vote/services/hooks';
+import CandidateStoresViewModel from '@/feature/vote/viewModels/CandidateStoresViewModel';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { voteQueryOptions } from '@/feature/vote/services/options/queries';
 
 type RTCSessionId = string;
 
 const useGetCandidateStores = (sessionId: RTCSessionId) => {
-  const { data: candidateStores } = useFetchCandidateStores<
-    CandidateStoreViewModelProps[]
-  >({
-    sessionId,
-    selector: (serverData) => {
-      return serverData.map((store) => ({
-        mainMenuName: store.menuName,
-        mainMenuImageUrl: store.menuImage.url,
-        storeName: store.storeName,
-        storeId: store.storeId,
-      }));
-    },
-  });
+  const { data: candidateStores } = useSuspenseQuery(
+    voteQueryOptions.suspense.fetchCandidateStores.query({
+      sessionId,
+      selector: (serverData) => {
+        return serverData.map((store) => ({
+          mainMenuName: store.menuName,
+          mainMenuImageUrl: store.menuImage.url,
+          storeName: store.storeName,
+          storeId: store.storeId,
+        }));
+      },
+    }),
+  );
 
   const candidateStoresViewModels = candidateStores.map((store) => {
     return new CandidateStoresViewModel(store);
